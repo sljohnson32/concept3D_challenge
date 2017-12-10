2ac53174339e27e1e74eee94fc8569e285d772b6
sljohnson32 <sljohnson32@gmail.com>
2017-12-10 15:13:57 -0700

    Add store handlers for grabbing lat and lng off map click

---
+++ src/reducers/LatLngReducer.js
@@ -0,0 +1,12 @@
+const LatLng = (state = [], action) => {
+
+  switch (action.type) {
+    case 'SET_LATLNG':
+      return action.coords;
+
+    default:
+      return state;
+  }
+};
+
+export default LatLng;
