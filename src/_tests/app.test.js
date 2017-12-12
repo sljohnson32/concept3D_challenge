7ed0f4bf1c87f15551c29335c2cf2c50dc516c4a
sljohnson32 <sljohnson32@gmail.com>
2017-12-12 15:28:02 -0700

    Setup tests

--- src/_tests/app.test.js
+++ src/_tests/app.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
 import App from '../components/App';
-import fakeLocations from './testLocations'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App locations={fakeLocations}/>, div);
+const mockFunc = jest.fn();
+
+
+describe('App', () => {
+
+  it('renders without crashing', () => {
+    shallow(<App fetchAllLocations={ mockFunc } />);
+  });
 });
